99388252331cc56587eed734a878b3b890310b80
Wahid Ali <wahidali8566@gmail.com>
2024-11-23 19:15:22 +0530

    DB connect

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
 dotenv.config({});
 
@@ -15,5 +16,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server running on port ${PORT}`);
 });
