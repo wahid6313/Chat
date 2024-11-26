89576abc193f1ba724071b3d6ecdffb5cd502b58
Wahid Ali <wahidali8566@gmail.com>
2024-11-26 18:29:55 +0530

    signUp section

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+        <h1 className="text-4xl text-center font-semibold text-white mb-7">
+          SignIn
+        </h1>
+        <form action="">
+          <div>
+            <label className="label">
+              <span className="text-base label-text text-white ">UserName</span>
+            </label>
+            <input
+              type="text"
+              placeholder="username"
+              className="w-full h-10 input input-bordered text-white"
+            ></input>
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text text-white ">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="w-full h-10 input input-bordered text-white"
+            ></input>
+          </div>
+
+          <div className=" text-center mt-7">
+            <button className="w-full bg-white h-9 rounded-lg text-black hover:bg-gray-200">
+              SignIn
+            </button>
+          </div>
+          <div className="text-center mt-4">
+            <p>
+              Create new account ?
+              <Link to="/register">
+                {" "}
+                <span className="hover:text-white hover:underline">SignUp</span>
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
