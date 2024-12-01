3e1b59aa42f30ceef998eb60b92a5d6d17f5ba51
Wahid Ali <wahidali8566@gmail.com>
2024-12-01 19:54:56 +0530

    signup section

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      userName: "",
+      password: "",
+    });
+  };
   return (
     <div className="min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
         <h1 className="text-4xl text-center font-semibold text-white mb-7">
           SignIn
         </h1>
-        <form action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className="label">
               <span className="text-base label-text text-white ">UserName</span>
             </label>
             <input
+              value={user.userName}
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
               type="text"
               placeholder="username"
               className="w-full h-10 input input-bordered text-white"
@@ -24,6 +39,8 @@ function Login() {
               <span className="text-base label-text text-white ">Password</span>
             </label>
             <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               type="password"
               placeholder="password"
               className="w-full h-10 input input-bordered text-white"
@@ -31,7 +48,10 @@ function Login() {
           </div>
 
           <div className=" text-center mt-7">
-            <button className="w-full bg-white h-10 rounded-lg text-black hover:bg-gray-300">
+            <button
+              type="submit"
+              className="w-full bg-white h-10 rounded-lg text-black hover:bg-gray-300"
+            >
               SignIn
             </button>
           </div>
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [user, setUser] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const checkboxHandler = (gender) => {
+    setUser({ ...user, gender });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      fullName: "",
+      userName: "",
+      password: "",
+      confirmPassword: "",
+      gender: "",
+    });
+  };
   return (
     <div className="min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
         <h1 className="text-4xl text-center font-semibold text-white mb-6">
           SignUp
         </h1>
-        <form action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className="label ">
               <span className="text-base label-text text-white ">FullName</span>
             </label>
             <input
+              value={user.fullName}
+              onChange={(e) => setUser({ ...user, fullName: e.target.value })}
               type="text"
               placeholder="fullname"
               className="w-full h-10 input input-bordered text-white"
@@ -24,6 +49,8 @@ function SignUp() {
               <span className="text-base label-text text-white ">UserName</span>
             </label>
             <input
+              value={user.userName}
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
               type="text"
               placeholder="username"
               className="w-full h-10 input input-bordered text-white"
@@ -34,6 +61,8 @@ function SignUp() {
               <span className="text-base label-text text-white ">Password</span>
             </label>
             <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               type="password"
               placeholder="password"
               className="w-full h-10 input input-bordered text-white"
@@ -46,6 +75,10 @@ function SignUp() {
               </span>
             </label>
             <input
+              value={user.confirmPassword}
+              onChange={(e) =>
+                setUser({ ...user, confirmPassword: e.target.value })
+              }
               type="password"
               placeholder="confirm password"
               className="w-full h-10 input input-bordered text-white"
@@ -54,15 +87,30 @@ function SignUp() {
           <div className="mt-4 flex items-center">
             <div className="flex items-center">
               <p className="text-white">Male</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input
+                checked={user.gender === "male"}
+                onChange={() => checkboxHandler("male")}
+                type="checkbox"
+                defaultChecked
+                className="checkbox mx-2"
+              />
             </div>
             <div className="flex items-center ml-6">
               <p className="text-white">Female</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input
+                checked={user.gender === "female"}
+                onChange={() => checkboxHandler("female")}
+                type="checkbox"
+                defaultChecked
+                className="checkbox mx-2"
+              />
             </div>
           </div>
           <div className=" text-center mt-7">
-            <button className="w-full bg-white h-9 rounded-lg text-black hover:bg-gray-300">
+            <button
+              type="submit"
+              className="w-full bg-white h-9 rounded-lg text-black hover:bg-gray-300"
+            >
               SignUp
             </button>
           </div>
